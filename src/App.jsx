341a93c6ef406ec3de1bba494169c7273ab24a7b
asfundmirza <asfundmirzaj@gmail.com>
2024-08-31 01:44:28 +0500

    added configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center mx-auto w-full min-h-screen">
+      <p className=" text-red-600">hello</p>
+    </div>
+  );
+}
+
+export default App;

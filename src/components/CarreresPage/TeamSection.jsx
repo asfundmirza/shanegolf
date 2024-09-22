3282f3e411e508766a7ebcff4cb7a5148cc1002a
asfundmirza <asfundmirzaj@gmail.com>
2024-09-22 06:25:33 +0500

    added some changes

--- src/components/CarreresPage/TeamSection.jsx
+++ src/components/CarreresPage/TeamSection.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const TeamSection = () => {
   return (
     <section>
-      {/* Container */}
       <div className="mx-auto w-full max-w-[95rem] px-5 py-8 md:px-10 md:py-8 md:pt-20">
-        {/* Title */}
         <h2 className="text-center text-3xl font-bold md:text-5xl">
           Join Our <span className="text-green-600">Team</span>
         </h2>
@@ -13,9 +11,7 @@ const TeamSection = () => {
           Be part of a forward-thinking company where your contributions make a
           real impact.
         </p>
-        {/* Content */}
         <div className="grid grid-cols-1 justify-items-center gap-5 sm:grid-cols-2 sm:justify-items-stretch md:grid-cols-3 md:gap-4 lg:gap-6">
-          {/* Item */}
           <div className="mx-auto flex w-full flex-col items-center gap-4 py-8 text-center md:px-8 md:py-4 lg:px-12">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
@@ -25,7 +21,6 @@ const TeamSection = () => {
             <p className="font-bold">John Robert</p>
             <p className="text-center text-sm text-gray-500">Web Developer</p>
           </div>
-          {/* Item */}
           <div className="mx-auto flex w-full flex-col items-center gap-4 py-8 text-center md:px-8 md:py-4 lg:px-12">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
@@ -35,7 +30,6 @@ const TeamSection = () => {
             <p className="font-bold">Annisyah Matah</p>
             <p className="text-center text-sm text-gray-500">Designer</p>
           </div>
-          {/* Item */}
           <div className="mx-auto flex w-full flex-col items-center gap-4 py-8 text-center md:px-8 md:py-4 lg:px-12">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
@@ -47,7 +41,6 @@ const TeamSection = () => {
               UI//UX&nbsp;Designer
             </p>
           </div>
-          {/* Item */}
           <div className="mx-auto flex w-full flex-col items-center gap-4 py-8 text-center md:px-8 md:py-4 lg:px-12">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
@@ -57,7 +50,6 @@ const TeamSection = () => {
             <p className="font-bold">Kevin Mason</p>
             <p className="text-center text-sm text-gray-500">Product Manager</p>
           </div>
-          {/* Item */}
           <div className="mx-auto flex w-full flex-col items-center gap-4 py-8 text-center md:px-8 md:py-4 lg:px-12">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
@@ -67,7 +59,6 @@ const TeamSection = () => {
             <p className="font-bold">Amy Parker</p>
             <p className="text-center text-sm text-gray-500">React Developer</p>
           </div>
-          {/* Item */}
           <div className="mx-auto flex w-full flex-col items-center gap-4 py-8 text-center md:px-8 md:py-4 lg:px-12">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
